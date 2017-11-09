c074f013aaeb64a98c7d37621cc8866d261d26fc
DrZolton <jr408614@ohio.edu>
2017-11-09 18:22:27 -0500

    added gravity.h

---
+++ gravity.h
@@ -0,0 +1,15 @@
+#ifndef GRAVITY_H
+#define GRAVITY_H
+
+double const sunG = 274.0;
+double const mercuryG = 3.7;
+double const venusG = 8.87;
+double const earthG = 9.807;
+double const moonG = 1.622;
+double const marsG = 3.711;
+double const jupiterG = 24.79;
+double const saturnG = 10.44;
+double const uranusG = 8.87;
+double const neptuneG = 11.15;
+
+#endif
