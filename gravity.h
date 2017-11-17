966207058bb3a9a24a60de2cc6c61c0485e96f17
Nat <is116615@ohio.edu>
2017-11-17 17:01:40 -0500

    Created vector struct for vector format

---
+++ gravity.h
@@ -0,0 +1,34 @@
+/********************************************************************************                
+Authors:      	Jacob Ramsey                                                  
+                Ohio University, B.S.C.S. May 2020                             
+	        jr408614@ohio.edu   
+		
+		Ignatius Smith
+		Ohio University, B.S.C.S. May 2020
+		is116615@ohio.edu
+
+		Alya ElGamal
+
+		Charlie Harover
+                                            
+File:           gravity.h                                                         
+Description:    This file includes the acceleration due to gravity (in m/s^2) for
+		several bodies in the solar system.
+Last Modified:  09 November 2017                                                
+********************************************************************************/
+
+#ifndef GRAVITY_H
+#define GRAVITY_H
+
+double const sunG = 274.0;
+double const mercuryG = 3.7;
+double const venusG = 8.87;
+double const earthG = 9.807;
+double const moonG = 1.622;
+double const marsG = 3.711;
+double const jupiterG = 24.79;
+double const saturnG = 10.44;
+double const uranusG = 8.87;
+double const neptuneG = 11.15;
+
+#endif
---
+++ motion.h
@@ -0,0 +1,103 @@
+/********************************************************************************                
+Authors:      	Ignatius Smith                                                  
+                Ohio University, B.S.C.S. May 2020                             
+	        is116615@ohio.edu   
+		
+		Jacob Ramsey
+
+		Alya ElGamal
+
+		Charlie Harover
+                                            
+File:           motion.h                                                         
+Description:    This file includes the function declarations for projectile motion
+		equations.                                             
+Last Modified:  09 November 2017                                                
+********************************************************************************/
+
+#include <cstdlib>
+#include <cmath>
+#ifndef MOTION_H
+#define MOTION_H
+
+using namespace std;
+
+struct vect{
+	double x_comp;
+	double y_comp;
+}
+
+class Motion{
+	public:
+		Motion();
+
+		/*Parameters for these functions are arbitrary and will need reconfigured*/
+		/*
+		parameters are defined as
+		iv - 	intial velocity magnitude
+		fv-	final velocity magnitude
+		ip-	inital position
+		fp-	final position
+		ac-	acceleration magnitude
+		th- 	theta
+		tm- 	time
+		*/
+
+		double calcAcc_X(double iv, double fv, double ip, double fp, double tm, double th);
+		double calcInitVelo_X(double ac, double fv, double ip, double fp, double tm, double th);
+		double calcFinalVelo_X(double iv, double ac, double ip, double fp, double tm, double th);
+		double calcInitPos_X(double iv, double fv, double ac, double fp, double tm, double th);
+		double calcFinalPos_X(double iv, double fv, double ip, double ac, double tm, double th);
+
+		double calcAcc_Y(double iv, double fv, double ip, double fp, double tm, double th);
+		double calcInitVelo_Y(double ac, double fv, double ip, double fp, double tm, double th);
+		double calcFinalVelo_Y(double iv, double ac, double ip, double fp, double tm, double th);
+		double calcInitPos_Y(double iv, double fv, double ac, double fp, double tm, double th);
+		double calcFinalPos_Y(double iv, double fv, double ip, double ac, double tm, double th);
+
+		double calcAcc_Vector(/*parameters*/);
+		double calcInitVelo_Vector(/*parameters*/);
+		double calcFinalVelo_Vector(/*parameters*/);
+
+		double calc_Time(double iv, double fv, double ip, double fp, double ac, double th);
+		double calc_Theta(double iv, double fv, double ip, double fp, double ac, double tm);
+
+		/*Parameters for these equations are correct*/
+
+		double getAcc_X();
+		double getInitVelo_X();
+		double getFinalVelo_X();
+		double getInitPos_X();
+		double getFinalPos_X();
+
+		double getAcc_Y();
+		double getInitVelo_Y();
+		double getFinalVelo_Y();
+		double getInitPos_Y();
+		double getFinalPos_Y();
+
+		double getTime();
+		double getTheta();
+
+	private:
+		double acceleration_x;
+		double intialvelocity_x;
+		double finalvelocity_x;
+		double intialposition_x;
+		double finalposition_x;
+
+		double acceleration_y;
+		double intialvelocity_y;
+		double finalvelocity_y;
+		double intialposition_y;
+		double finalposition_y;
+
+		vect acceleration_vector;
+		vect intialvelocity_vector;
+		vect finalvelocity_vector;
+
+		double time;
+		double theta;	
+}
+
+#endif
